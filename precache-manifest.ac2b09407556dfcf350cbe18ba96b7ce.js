89d340c9ac61ce1cfb14e7a6af8d9b665793bfd0
Robert Fox <robert@foxs.com>
2020-02-27 10:52:37 -0500

    Updates

---
+++ precache-manifest.ac2b09407556dfcf350cbe18ba96b7ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bd884d2fa90427e695d6386742c5a5d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8aa653f29f5cf666450d",
+    "url": "/portfolio/static/css/2.debc53f4.chunk.css"
+  },
+  {
+    "revision": "5000be45226c9718fee6",
+    "url": "/portfolio/static/css/main.5e4653e1.chunk.css"
+  },
+  {
+    "revision": "8aa653f29f5cf666450d",
+    "url": "/portfolio/static/js/2.6d6f995d.chunk.js"
+  },
+  {
+    "revision": "b9e194fc251592a86e3cbfa4659be782",
+    "url": "/portfolio/static/js/2.6d6f995d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5000be45226c9718fee6",
+    "url": "/portfolio/static/js/main.b2a16ec9.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  }
+]);
