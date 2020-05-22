801b76e4a0c8b55436f514b20a0886892e26ee15
Robert Fox <robert@foxs.com>
2020-05-22 09:20:07 -0400

    Updates

---
+++ precache-manifest.b7ed8ab030888cf37f216ad4da1c3969.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bcc064d4d28c8f19c3a8c45d043fbf9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "496e067b2b3cd677bc56",
+    "url": "/portfolio/static/css/2.debc53f4.chunk.css"
+  },
+  {
+    "revision": "0b55a3d3244e467fd5da",
+    "url": "/portfolio/static/css/main.c81e8e31.chunk.css"
+  },
+  {
+    "revision": "496e067b2b3cd677bc56",
+    "url": "/portfolio/static/js/2.19f8290d.chunk.js"
+  },
+  {
+    "revision": "7593b734cb73132d3844d48ee6ed9e41",
+    "url": "/portfolio/static/js/2.19f8290d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b55a3d3244e467fd5da",
+    "url": "/portfolio/static/js/main.bf24fb4c.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.f52bd0e594763463855c7f6ba3269a11.js"
+  "/portfolio/precache-manifest.b7ed8ab030888cf37f216ad4da1c3969.js"
 );
 
 self.addEventListener('message', (event) => {
